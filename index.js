4bb3c8870a247d1cb4f30cc527da0e90724dcc7a
TudbuT <tudbut@tudbut.de>
2022-01-01 07:14:33 +0100

    Support for more channel names

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const irc = new IRC(ip, nick)
 
 irc.on('message', async function (from, to, message) {
     console.log(from + ' => ' + to + ': ' + message)
-    let channel = client.channels.cache.find(channel => ('#' + channel.name.toLowerCase()) === to)
+    let channel = client.channels.cache.find(channel => ('#' + channel.name.toLowerCase()) === to || channel.topic === to)
     if (channel && channel.type === 'GUILD_TEXT') {
         if (!channel.webhook) {
             channel.webhook = 0
@@ -47,8 +47,8 @@ client.on('messageCreate', async function (message) {
     if (message.webhookId)
         return
     if (message.channel.type === 'GUILD_TEXT') {
-        irc.join('#' + message.channel.name)
-        irc.say('#' + message.channel.name, message.content) 
+        irc.join(message.channel.topic | ('#' + message.channel.name))
+        irc.say(message.channel.topic('#' + message.channel.name), message.content) 
     }
 })
 
