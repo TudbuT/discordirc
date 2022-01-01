4d348400306e7731272dfed0c10abdddb8a3adf0
TudbuT <tudbut@tudbut.de>
2022-01-01 16:54:08 +0100

    Add ONJOIN variable, Add BRIDGE functionality

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const ip =      process.env.IP
 const port =    process.env.PORT | 6667
 const token =   process.env.TOKEN
 const nick =    process.env.NICK
+const bridge =  process.env.BRIDGE === '1'
+const onjoin =  process.env.ONJOIN
 
 
 
@@ -22,11 +24,17 @@ const irc = new IRC(ip, nick)
 client.login(token)
 setTimeout(() => {
     client.guilds.cache.forEach(it => it.channels.fetch())
-    client.channels.cache.forEach(channel => {
+    if (onjoin) {
+        let msgs = onjoin.split(';')
+        for (let msg of msgs) {
+            (eval('function _(irc) {irc.send("' + msg.split(' ').join('", "') + '")} ; _'))(irc)
+        }
+    }
+    setTimeout(() => client.channels.cache.forEach(channel => {
         if (channel.topic) {
             irc.join(channel.topic)
         }
-    })
+    }), 5000)
 }, 5000)
 
 irc.on('message', async function (from, to, message) {
@@ -52,10 +60,14 @@ client.on('messageCreate', async function (message) {
     if (message.webhookId)
         return
     if (message.channel.type === 'GUILD_TEXT') {
+        let content = message.cleanContent
+        if (bridge) {
+            content = message.author.username + ': ' + content
+        }
         let channel = message.channel.topic ? message.channel.topic : ('#' + message.channel.name)
-        console.log(channel + ' <= ' + message.content.split('\n').join('\n' + channel + ' <= '))
+        console.log(channel + ' <= ' + content.split('\n').join('\n' + channel + ' <= '))
         irc.join(channel)
-        irc.say(channel, message.cleanContent) 
+        irc.say(channel, content) 
     }
 })
 
